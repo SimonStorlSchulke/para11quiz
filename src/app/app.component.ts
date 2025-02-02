3f786ea98e65051702d9cf5643f665ed88fdabc0
SimonStorlSchulke <simonstoschu@googlemail.com>
2025-02-03 00:34:39 +0100

    redeploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { AsyncPipe } from '@angular/common';
 export class AppComponent implements OnInit {
   title = 'para11quiz';
 
-  questions = [...allQuestions];
+  questions = [allQuestions[0], allQuestions[1], allQuestions[2], allQuestions[3]];
 
   streakMap = new Map<number, number>();
   currentQuestionIndex = this.getBiasedRandomQuestionId();
